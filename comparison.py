27bf55d00fd22fd7a9c34f4cd19282115d8f1a0a
Sudarat1003 <52315113+Sudarat1003@users.noreply.github.com>
2019-09-13 16:33:16 +0700

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,7 @@
+print("Equality	:\t",nil,"==",num,nil==num) 
+print("Equality	:\t",cap,"==",low,cap==low) 
+print("Inequality:\t",nil,"!=",	max,nil!=max) 
+print("Greater	:\t",nil,">",max,nil>max) 
+print("Lesser:\t",nil,"<",max,nil<max)
+print("MoreOrEqual:\t",	nil,">=",num,nil>=num) 
+print("Less or Equal:\t",max,"<=",num,max<=num) 
